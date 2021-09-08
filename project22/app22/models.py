ab9d3181b722b2865c8151feeab3338371758c6b
Netravati <netravati1000@gmail.com>
2021-09-08 21:42:36 +0530

    cookies example

---
+++ project22/app22/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class ProductModel(models.Model):
+    number = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100,null=False,unique=True)
+    price = models.FloatField()
+    photo = models.ImageField(upload_to='product_images/')
+
